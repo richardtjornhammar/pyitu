2c8e0ba8509da2ff70225529685bb9bea2a2f87a
pts-rictjo <richard.tjornhammar@pts.se>
2025-10-16 16:36:03 +0200

    Add build_table function to create DataFrame
    Added a function to build a DataFrame from a CSV-like string.

--- src/pyitu/tooling.py
+++ src/pyitu/tooling.py
@@ -42,3 +42,9 @@ def marshall_palmer_DSD(D_mm, R_mmph):
 def fuzzy_find(v,arr) :
     idx = sorted( [(f,i) for (i,f) in zip( range(len(arr)),np.abs(arr-v) ) ] ,key = lambda x:x[0] )[0][1]
     return idx
+
+def build_table(table):
+   table = [v.split(',') for v in table.split('\n')]
+   table = pd.DataFrame(table[1:],columns=table[0])
+   table.index = table.iloc[:,0]
+   return ( table )
